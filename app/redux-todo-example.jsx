55e5404e3ee9ed46640a45e854702c2c77a08287
David Alberto Hans Castillo <dhansc@bch.bancodechile.cl>
2017-11-14 16:22:57 -0300

    ADD: reducers múltiples

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,7 +9,7 @@ let stateDefault = {
 };
 
 let count = 0;
-let reducer = (state = stateDefault, action) => {
+let oldReducer = (state = stateDefault, action) => {
 	switch(action.type){
 		case 'CHANGE_SEARCH_TEXT':
 			return {
@@ -24,10 +24,53 @@ let reducer = (state = stateDefault, action) => {
 					{id: count++,text: action.todo}
 				]
 			};
+		case 'REMOVE_TODO':
+			return {
+				...state,
+				todos: state.todos.filter((todo) => todo.id !== action.id)
+			};
 		default:
 			return state;
 	}
 };
+
+let searchTextReducer = (state = '', action) => {
+	switch(action.type){
+		case 'CHANGE_SEARCH_TEXT':
+			return action.searchText;
+		default:
+			return state;
+	}
+}
+
+let todosReducer = (state = [], action) => {
+	switch(action.type){
+		case 'ADD_TODO':
+			return [
+				...state,
+				{id: count++,text: action.todo}
+			];
+		case 'REMOVE_TODO':
+			return state.filter((todo) => todo.id !== action.id);
+		default:
+			return state;
+	}
+};
+
+let showCompletedReducer = (state = false, action) => {
+	switch(action.type){
+		case 'CHANGE_SHOW_COMPLETED':
+			return !state;
+		default:
+			return state;
+	}
+};
+
+let reducer = redux.combineReducers({
+	searchText: searchTextReducer,
+	todos: todosReducer,
+	showCompleted: showCompletedReducer
+});
 
 let store = redux.createStore(reducer, redux.compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -54,10 +97,18 @@ let addTodo = (todo) => {
 	});
 }
 
+let removeTodo = (id) => {
+	store.dispatch({
+		type: 'REMOVE_TODO',
+		id: id
+	});
+}
+
 changeSearchText('Hello');
 changeSearchText('World');
 changeSearchText('!');
 addTodo('Hello');
 addTodo('World');
 addTodo('!');
+removeTodo(1);
 
