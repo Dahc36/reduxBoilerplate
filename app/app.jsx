cf3d085ee98a1827df52378c6f58f2aa23eb926b
David Alberto Hans Castillo <dhansc@bch.bancodechile.cl>
2017-11-17 16:59:30 -0300

    ADD: reducers

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ let ReactDOM = require('react-dom');
 let {Route, Router, IndexRoute, hashHistory} = require('react-router');
 let TodoApp = require('TodoApp');
 
+let actions = require('configureStore');
+
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
@@ -12,7 +14,7 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
 	<div>
-		
+		<TodoApp/>
 	</div>,
 	document.getElementById('app')
 );
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -9,7 +9,6 @@ export let searchTextReducer = (state = '', action) => {
 	}
 }
 
-let count = 0;
 export let todosReducer = (state = [], action) => {
 	switch(action.type){
 		case 'ADD_TODO':
@@ -18,15 +17,16 @@ export let todosReducer = (state = [], action) => {
 				{id: uuid(), text: action.text, completed: false}
 			];
 		case 'TOGGLE_TODO':
-			let tempTodo = state.filter((todo) => todo.id === action.id)[0]
-			return 
-			[
-				...state.filter((todo) => todo.id !== action.id),
-				{
-					...tempTodo,
-					completed: !tempTodo.comlpeted
+			return state.map((todo) => {
+				if(todo.id === action.id){
+					return {
+						...todo,
+						completed: !todo.completed;
+					};
+				} else {
+					return todo;
 				}
-			];
+			});
 		default:
 			return state;
 	}
